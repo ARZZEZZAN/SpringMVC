4198388f4c9cebcdf0573bb0e9ada79bd4a3e693
Killer Albertha <killeral@va-g3.kzn.21-school.ru>
2023-05-20 11:22:30 +0300

    f

---
+++ MVC/src/main/java/edu/armenarzz/spring/ControllerApp.java
@@ -0,0 +1,12 @@
+package edu.armenarzz.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControllerApp {
+    @GetMapping("/hello-world")
+    public String hello() {
+        return "hello_world";
+    }
+}
