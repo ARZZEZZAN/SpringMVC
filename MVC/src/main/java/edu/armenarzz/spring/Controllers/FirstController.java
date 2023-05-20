e9c2ea43d3e69bc98c677125b787cff8ad638a26
Killer Albertha <killeral@va-g3.kzn.21-school.ru>
2023-05-20 19:19:50 +0300

    Server works

--- MVC/src/main/java/edu/armenarzz/spring/Controllers/FirstController.java
+++ MVC/src/main/java/edu/armenarzz/spring/Controllers/FirstController.java
@@ -1,12 +1,18 @@
 package edu.armenarzz.spring.Controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message", name + " " + surname);
         return "controller/hello";
     }
     @GetMapping("/bye")
